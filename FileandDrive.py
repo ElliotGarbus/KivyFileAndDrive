750ef4d60959eb0edecf311c1d93a89213c6c3cb
ElliotGarbus <elliotg2@cox.net>
2019-06-04 13:50:23 -0700

    added properties

--- FileandDrive.py
+++ FileandDrive.py
@@ -1,5 +1,7 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.popup import Popup
+from kivy.properties import StringProperty, ListProperty
 
 
 class TestBox(BoxLayout):
@@ -8,6 +10,26 @@ class TestBox(BoxLayout):
 
 class FCTest(App):
     pass
+
+
+class LoadDialog(Popup):
+    filters = ListProperty()
+    path = StringProperty()
+
+    def __init__(self, **kwargs):
+        self.filters = kwargs['filters']
+        self.path= kwargs['path']
+        super().__init__(**kwargs)
+
+
+class SaveDialog(Popup):
+    filters = ListProperty()
+    path = StringProperty()
+
+    def __init__(self, **kwargs):
+        filters = kwargs['filters']
+        path= kwargs['path']
+        super().__init__(**kwargs)
 
 
 if __name__ == '__main__':
