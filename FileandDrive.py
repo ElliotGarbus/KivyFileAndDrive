46787271f57808b378685b8d373cbbce9d640815
Elliot Garbus <elliotg2@cox.net>
2019-06-03 17:29:29 -0700

    linux support added

--- FileandDrive.py
+++ FileandDrive.py
@@ -12,6 +12,7 @@ class TestBox(BoxLayout):
 
 
 class FCTest(App):
+
     def get_win_drive_names(self):
         volumeNameBuffer = create_unicode_buffer(1024)
         fileSystemNameBuffer = create_unicode_buffer(1024)
@@ -28,7 +29,7 @@ class FCTest(App):
                                                        serial_number, max_component_length, file_system_flags,
                                                        fileSystemNameBuffer, sizeof(fileSystemNameBuffer))
             if rc:
-                drive_names.append(f'{volumeNameBuffer.value}({d[:2]})')
+                drive_names.append(f'{volumeNameBuffer.value}({d[:2]})')  # disk_name(C:)
         return drive_names
 
     def get_drive_names(self):
@@ -36,10 +37,18 @@ class FCTest(App):
             names = self.get_win_drive_names()
         elif platform == 'macosx':
             names = listdir('/Volumes')
-        else:
-            names = ['ERROR']  # raise exception?
+        elif platform == 'linux':
+            names = ['/']
         return names
 
+    def get_path(self, spinner_text):
+        if platform == 'win':
+            new_path = spinner_text[-3:-1] + '/'
+        elif platform == 'macosx':
+            new_path = '/Volumes/' + spinner_text
+        elif platform == 'linux':
+            new_path = spinner_text
+        return new_path
 
 if __name__ == '__main__':
     FCTest().run()
